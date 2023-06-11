e62de127fe0b707559d822c22e2120d7b15e549f
Juliao-Martins <juliaodejesusmartins@gmail.com>
2023-06-11 19:54:24 +0700

    Adding game board.

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ import textConsole from "./colorfull.js";
 //   fontSize: "3em",
 //   style: "normal",
 // });
+
+// ! initial all here
+
+// canvas
+let canvas = "",
+  context = "",
+  cols = 20,
+  rows = 20,
+  blockSize = 25;
+
+// DOMContentLoaded
+window.addEventListener("DOMContentLoaded", (e) => {
+  canvas = document.getElementById("canvas");
+  canvas.width = cols * blockSize;
+  canvas.height = rows * blockSize;
+  context = canvas.getContext("2d");
+
+  update(); // ! invoke update function here
+});
+
+// all thing will load here
+// inside function
+function update() {
+  context.fillStyle = "#000";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+}
